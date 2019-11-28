4f732885190b0b2f2401d019d852a541bc695f74
Ian Corne <ian.corne.be@gmail.com>
2019-11-28 10:42:46 +0100

    Ex 3: pipe

--- src/app/shared/pipes/strength.pipe.spec.ts
+++ src/app/shared/pipes/strength.pipe.spec.ts
@@ -1,3 +1,24 @@
+import { StrengthPipe } from './strength.pipe';
+
 describe('StrengthPipe', () => {
+  let pipe: StrengthPipe;
+  beforeEach(() => {
+    pipe = new StrengthPipe();
+  });
+  it('transforms < 10 to a weak annotated value', () => {
+    const value = 9;
+    const actual = pipe.transform(value);
+    expect(actual).toEqual(`${value} (weak)`);
+  });
+  it('transforms >= 10, <20 to a strong annotated value', () => {
+    const value = 19;
+    const actual = pipe.transform(value);
+    expect(actual).toEqual(`${value} (strong)`);
+  });
+  it('transforms >= 20 to a unbelievable annotated value', () => {
+    const value = 20;
+    const actual = pipe.transform(value);
+    expect(actual).toEqual(`${value} (unbelievable)`);
+  });
 
 });
