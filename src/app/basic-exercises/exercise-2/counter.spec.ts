84963a14d1fdd1cbca64cd4fd5d1edfd0a8b112a
Ian Corne <ian.corne.be@gmail.com>
2019-11-28 10:11:58 +0100

    exercise two done

--- src/app/basic-exercises/exercise-2/counter.spec.ts
+++ src/app/basic-exercises/exercise-2/counter.spec.ts
@@ -1,5 +1,42 @@
-import {Counter} from './counter';
+import { of } from 'rxjs';
+import { Counter } from './counter';
 
 describe('Counter2', () => {
 
+  let counter: Counter;
+  let apiMock;
+  beforeEach(() => {
+    apiMock = {
+      getMessage: jest.fn(),
+    };
+    apiMock.getMessage.mockImplementation(() => of('HELLO WORLD'));
+
+    counter = new Counter(apiMock);
+  });
+  test('should be init with 0', async () => {
+    const actual = await counter.getCounter();
+    expect(actual).toBe(0);
+  });
+  test('Increasing the counter three times should result in the counter having a count of 3.', async () => {
+    await counter.increase();
+    await counter.increase();
+    await counter.increase();
+    const actual = await counter.getCounter();
+    expect(actual).toBe(3);
+  });
+  test('Decreasing it once and then increasing it three times should result in the counter having a count of 3.', async () => {
+    await counter.decrease();
+    await counter.increase();
+    await counter.increase();
+    await counter.increase();
+    const actual = await counter.getCounter();
+    expect(actual).toBe(3);
+  });
+  test('Increasing the counter twice and resetting it should result in the counter having a count of 0.', async () => {
+    await counter.increase();
+    await counter.increase();
+    await counter.reset();
+    const actual = await counter.getCounter();
+    expect(actual).toBe(0);
+  });
 });
