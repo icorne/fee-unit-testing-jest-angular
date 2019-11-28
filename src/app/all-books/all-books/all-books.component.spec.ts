d910cde3eba8d3060facc67a6195a0b856829526
Ian Corne <ian.corne.be@gmail.com>
2019-11-28 12:44:27 +0100

    test all books component

--- src/app/all-books/all-books/all-books.component.spec.ts
+++ src/app/all-books/all-books/all-books.component.spec.ts
@@ -1,17 +1,18 @@
 import { TranslateService } from '@ngx-translate/core';
-import { Observable, of, throwError } from 'rxjs';
+import { of, throwError } from 'rxjs';
+import { finalize } from 'rxjs/operators';
 import { Book } from '../../shared/domain/book.model';
 import { BookService } from '../../shared/services/book.service';
 import { MessageService } from '../../shared/services/message.service';
-import { MyBooksComponent } from './my-books.component';
+import { AllBooksComponent } from './all-books.component';
 
-describe('MyBooksComponent', () => {
-  let booksComponent: MyBooksComponent;
+describe('AllBooksComponent', () => {
+  let allBooksComponent: AllBooksComponent;
   let bookServiceMock: { [key: string]: jest.Mock<BookService> | any };
   let messageServiceMock: { [key: string]: jest.Mock<MessageService> | any };
   let translateServiceMock: { [key: string]: jest.Mock<TranslateService> | any };
   let books: Book[];
-  beforeEach(() => {
+  beforeEach(async () => {
     books = [
       {
         amountOfPages: 1,
@@ -25,8 +26,8 @@ describe('MyBooksComponent', () => {
       },
     ];
     bookServiceMock = {
-      getMyBooks: jest.fn((): Observable<Book[]> => of(books)),
-      returnBook: jest.fn(),
+      borrowBook: jest.fn(),
+      getAllBooks: jest.fn(),
     };
     messageServiceMock = {
       showSuccessMessage: jest.fn(),
@@ -35,66 +36,73 @@ describe('MyBooksComponent', () => {
     translateServiceMock = {
       instant: jest.fn((key) => key),
     };
-    booksComponent = new MyBooksComponent(
+    allBooksComponent = new AllBooksComponent(
       bookServiceMock as any as BookService,
       messageServiceMock as any as MessageService,
       translateServiceMock as any as TranslateService,
     );
-    booksComponent.bookList = books;
+    const booksGotten = new Promise((resolve) => {
+      bookServiceMock.getAllBooks.mockImplementation(() => {
+        return of(books)
+          .pipe(finalize(resolve));
+      });
+    });
+    allBooksComponent.ngOnInit();
+    await booksGotten;
   });
   it('constructs', () => {
-    expect(booksComponent).toBeDefined();
+    expect(allBooksComponent).toBeDefined();
+  });
+  it('sets the books', () => {
+    expect(allBooksComponent.books).toBe(books);
   });
 
-  describe('.handIn()', () => {
-    let msHasBeenCalled: Promise<void>;
+  describe('.borrow(book)', () => {
+    let bookToBorrow: Book;
     beforeEach(() => {
-      msHasBeenCalled = new Promise((resolve) => {
-        messageServiceMock.showSuccessMessage.mockImplementation(() => resolve());
-        messageServiceMock.showErrorMessage.mockImplementation(() => resolve());
-      });
+      bookToBorrow = books[0];
     });
     describe('success', () => {
       const successTranslation = 'success';
       const handedInTranslation = 'good';
-      let bookToHandIn: Book;
-      beforeEach(async () => {
-        bookServiceMock.returnBook.mockImplementation((book) => of(book));
+      beforeEach((done) => {
+        bookServiceMock.borrowBook.mockImplementation((book) => of(book).pipe(finalize(done)));
         translateServiceMock.instant.mockImplementation((key) => key === 'SUCCESS' ? successTranslation : handedInTranslation);
-        bookToHandIn = books[0];
-        booksComponent.handIn(bookToHandIn);
-        await msHasBeenCalled;
+        allBooksComponent.borrow(bookToBorrow);
+      });
+      it('removes the book from all books', () => {
+        expect(allBooksComponent.books).not.toContain(bookToBorrow);
       });
       it('calls showSuccessMessage', () => {
         expect(messageServiceMock.showSuccessMessage).toHaveBeenCalledWith(successTranslation, handedInTranslation);
       });
       it('calls correct translation', () => {
-        expect(translateServiceMock.instant).toHaveBeenCalledWith('HANDED_IN', {
-          title: bookToHandIn.title,
-          author: bookToHandIn.author
+        expect(translateServiceMock.instant).toHaveBeenCalledWith('CHECKED_OUT', {
+          title: bookToBorrow.title,
+          author: bookToBorrow.author
         });
       });
     });
     describe('error', () => {
       const errorTranslation = 'error';
       const failTranslation = 'bad';
-      let bookToHandIn: Book;
       let message;
-      beforeEach(async () => {
+      beforeEach((done) => {
 
         message = 'I FAIL';
-        bookServiceMock.returnBook.mockImplementation(() => throwError(new Error(message)));
+        bookServiceMock.borrowBook.mockImplementation(() => throwError(new Error(message)).pipe(finalize(done)));
         translateServiceMock.instant.mockImplementation((key) => key === 'ERROR' ? errorTranslation : failTranslation);
-        bookToHandIn = books[0];
-        booksComponent.handIn(bookToHandIn);
-        await msHasBeenCalled;
+        allBooksComponent.borrow(bookToBorrow);
+      });
+      it('does not remove the book from all books', () => {
+        expect(allBooksComponent.books).toContain(bookToBorrow);
       });
       it('calls showErrorMessage', () => {
         expect(messageServiceMock.showErrorMessage).toHaveBeenCalledWith(errorTranslation, failTranslation);
       });
       it('calls correct translation', () => {
         expect(translateServiceMock.instant).toHaveBeenCalledWith('SOMETHING_WENT_WRONG', {
-          message,
+          message
         });
       });
     });
